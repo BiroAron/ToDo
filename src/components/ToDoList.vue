4a90aae2ddb27298f818a8afe3b85f3eff9b00be
Biró Áron <biro.aron000802@gmail.com>
2023-05-23 11:55:51 +0300

    A feladat elso resze

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+    import { ref } from 'vue'
+
+    var items = ref([])
+    const todoInput = ref('')
+
+    function addItem(){
+        const newItem = todoInput.value
+        if(newItem != '')
+        items.value.push(newItem)
+        todoInput.value = ''
+    }
+
+    function removeItem(index){
+        items.value.splice(index, 1)
+    }
+
+    function clear(){
+        items.value.splice(0)
+    }
+
+</script>
+
+<template>
+
+    <div id="clear">
+        <button @click="clear">Clear</button>
+    </div>
+    <div>
+        <input type = "text" placeholder="ToDo" v-model="todoInput"/>
+        <div>
+            <button @click="addItem">Add</button>
+        </div>
+    </div>
+    <ul >
+        <li v-for="(item, index) in items" :key="index">
+        {{ item }} <button @click="removeItem(index)">Remove</button>
+        </li>
+    </ul>
+
+</template>
