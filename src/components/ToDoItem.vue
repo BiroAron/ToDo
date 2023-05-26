3f0dde6606fd42939001e5a6329d94f2a2a42947
Biró Áron <biro.aron000802@gmail.com>
2023-05-26 15:56:30 +0300

    Second part corrected
    Added new components. Removed helloword.wue and some unnecessary css.

---
+++ src/components/ToDoItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="index">
+      {{ todo.title }} - {{ todo.text }} - {{ todo.priority }}
+      <button @click="deleteItem(todo)">Remove</button>
+    </li>
+  </ul>
+</template>
+
+<script setup lang='ts'>
+  defineProps(['todos']);
+
+  function deleteItem(todo: {index:number}){
+    emit('deleteItem', todo)
+  }
+
+  const emit = defineEmits(['deleteItem']);
+</script>
--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,41 +1,45 @@
-<script setup lang="ts">
-    import { ref } from 'vue'
-
-    var items = ref([])
-    const todoInput = ref('')
-
-    function addItem(){
-        const newItem = todoInput.value
-        if(newItem != '')
-        items.value.push(newItem)
-        todoInput.value = ''
-    }
-
-    function removeItem(index){
-        items.value.splice(index, 1)
-    }
-
-    function clear(){
-        items.value.splice(0)
-    }
-
-</script>
-
 <template>
-
-    <div id="clear">
-        <button @click="clear">Clear</button>
-    </div>
-    <div>
-        <input type = "text" placeholder="ToDo" v-model="todoInput"/>
-        <div>
-            <button @click="addItem">Add</button>
-        </div>
-    </div>
-    <ul >
-        <li v-for="(item, index) in items" :key="index">
-        {{ item }} <button @click="removeItem(index)">Remove</button>
-        </li>
-    </ul>
-
+	<div>
+		<!-- <Header @toggle-visibility="toggleVisibility"></Header> -->
+		<div :style="{ display: shouldShow ? 'block' : 'none' }">
+			<div id="clear">
+				<button @click="clear">Clear</button>
+			</div>
+			<ToDoForm @add-todo="addTodo" />
+		</div>
+		<ToDoItem :todos="todos" @delete-item="removeItem" />
+	</div>
 </template>
+
+<script setup lang="ts">
+	import { ref } from 'vue'
+	import ToDoForm from './ToDoForm.vue'
+	//import Header from './Header.vue'
+	import ToDoItem from './ToDoItem.vue'
+
+	interface Todo {
+		title: string;
+		priority: string;
+		text: string;
+	}
+
+	const shouldShow = ref(true);
+
+	const todos = ref<Todo[]>([]);
+
+	function addTodo(todo: Todo) {
+		todos.value.push(todo);
+	}
+
+	function removeItem(index: number) {
+		todos.value.splice(index, 1);
+	}
+
+	// function toggleVisibility(){
+	// 	shouldShow.value = !shouldShow.value;
+	// }
+
+	function clear() {
+		todos.value.splice(0);
+	}
+</script>
