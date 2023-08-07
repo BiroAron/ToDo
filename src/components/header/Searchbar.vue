4a9c18066a3633167b85426d6c12075bfb8480e7
Biró Áron <biro.aron000802@gmail.com>
2023-08-07 13:35:24 +0300

    ITL 22 - Make an input field and design it

--- src/components/header/Searchbar.vue
+++ src/components/header/Searchbar.vue
@@ -6,10 +6,27 @@
       <SearchIcon class="min-w-custom" />
       <input
         type="text"
+        v-model="searchQuery"
         class="flex-1 px-2 py-1 focus:outline-none w-full"
         placeholder="Search Todo"
+        @input="filterTodos"
       />
     </div>
     <button class="bg-black text-white px-3 py-1 rounded-lg">Search</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import SearchIcon from '../icons/SearchIcon.vue'
+
+const emit = defineEmits<{
+  (e: 'setSearchQuery', searchQuery: string): void
+}>()
+
+const searchQuery = ref('')
+
+function filterTodos() {
+  emit('setSearchQuery', searchQuery.value)
+}
+</script>
