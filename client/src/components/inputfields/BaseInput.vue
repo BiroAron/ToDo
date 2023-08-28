5c68e888270322d073165e9335919a8b469e1e3d
Biró Áron <biro.aron000802@gmail.com>
2023-08-28 15:03:45 +0300

    ILT 34 - Add a Sign Up page

---
+++ client/src/components/inputfields/BaseInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <label class="mb-1 block text-black">{{ inputName }}</label>
+  <input
+    class="mb-4 w-full rounded-lg border-2 border-black p-2 text-black outline-none focus:bg-gray-300"
+    :type="inputType"
+  />
+</template>
+
+<script setup lang="ts">
+interface Props {
+  inputName: string
+  inputType: string
+}
+defineProps<Props>()
+</script>
---
+++ client/src/views/SignUp.vue
@@ -0,0 +1,60 @@
+<template>
+  <body class="flex h-screen w-screen bg-white">
+    <div
+      class="m-auto w-full max-w-lg rounded-2xl border-4 border-black bg-[#fff7f7] p-5 shadow-xl"
+    >
+      <header>
+        <div
+          class="mb-8 flex justify-center text-6xl font-bold text-black opacity-80"
+        >
+          ToDo App
+        </div>
+      </header>
+      <form>
+        <div class="mb-2 flex flex-row space-x-4">
+          <div>
+            <BaseInput input-name="First name" input-type="text"></BaseInput>
+          </div>
+          <div>
+            <BaseInput input-name="Last name" input-type="text"></BaseInput>
+          </div>
+        </div>
+
+        <BaseInput input-name="Email" input-type="text"></BaseInput>
+        <BaseInput input-name="Password" input-type="password"></BaseInput>
+        <BaseInput
+          input-name="Repeat Password"
+          input-type="password"
+        ></BaseInput>
+        <div>
+          <RouterLink
+            class="mb-6 flex w-full cursor-pointer items-center justify-center rounded-full bg-primary px-4 py-2 text-xl font-bold text-white hover:bg-low"
+            :to="{ name: 'Dashboard' }"
+          >
+            Register
+          </RouterLink>
+        </div>
+      </form>
+      <footer>
+        <RouterLink
+          class="float-left text-sm text-black hover:text-low"
+          href="#"
+          :to="{ name: 'SignUp' }"
+          >Forgot Password?</RouterLink
+        >
+        <RouterLink
+          class="float-right text-sm text-black hover:text-low"
+          href="#"
+          :to="{ name: 'Login' }"
+          >Login</RouterLink
+        >
+      </footer>
+    </div>
+  </body>
+</template>
+
+<script setup lang="ts">
+import { RouterLink } from 'vue-router'
+import BaseButton from '../components/todo/BaseButton.vue'
+import BaseInput from '../components/inputfields/BaseInput.vue'
+</script>
