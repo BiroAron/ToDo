2f455efed7152d16e3ea4436be6dcc0685e3f070
Biró Áron <biro.aron000802@gmail.com>
2023-09-04 17:03:36 +0300

    ITL 33 - Create CRUD operations
    - also moved search and filtering functionality
    - implemented archive by making soft deletes

---
+++ server/src/controller/todos.ts
@@ -0,0 +1,115 @@
+import express from "express";
+import {
+  createTodo,
+  getTodosByCriteria,
+  sortAndFilterTodos,
+  updateTodoById,
+} from "../service/todo";
+import TodoModel, { Todo } from "../models/todo";
+import { get } from "lodash";
+import { sortTodos } from "../helpers";
+
+export async function addTodo(req: express.Request, res: express.Response) {
+  console.log("addTodo");
+  try {
+    const userId = get(req, "identity._id") as string;
+    const { title, description, isChecked, priority, date } = req.body;
+
+    const todo = await createTodo({
+      userId,
+      title,
+      description,
+      priority,
+      isChecked,
+      date,
+    });
+
+    return res.status(201).json(todo);
+  } catch (error) {
+    console.error("Error during registration:", error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function getTodos(req: express.Request, res: express.Response) {
+  console.log("getAllTodos");
+  try {
+    const userId = get(req, "identity._id") as string;
+    const query = req.query.query as string;
+    const filterBy = req.query.filter_by as string;
+    const isAscending = req.query.is_ascending === "true";
+
+    const todos = await getTodosByCriteria(userId, query);
+
+    const sortedAndFilteredTodos = sortAndFilterTodos(
+      todos,
+      filterBy,
+      isAscending
+    );
+
+    todos.splice(0, todos.length, ...sortedAndFilteredTodos);
+
+    return res.status(201).json(todos);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function getTodo(req: express.Request, res: express.Response) {
+  console.log("getSingleTodo");
+  try {
+    const userId = get(req, "identity._id") as string;
+    const todoId = req.params.id;
+
+    const todo = await TodoModel.findOne({ _id: todoId, userId });
+
+    if (!todo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    return res.json(todo);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function deleteTodo(req: express.Request, res: express.Response) {
+  console.log("deleteTodo");
+  try {
+    const userId = get(req, "identity._id") as string;
+    const todoId = req.params.id;
+
+    const todo = await TodoModel.findOne({ _id: todoId, userId });
+    if (!todo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+
+    todo.deleteDate = new Date();
+    await todo.save();
+
+    return res.json({ message: "Todo deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
+
+export async function updateTodo(req: express.Request, res: express.Response) {
+  console.log("updateTodo");
+  try {
+    const userId = get(req, "identity._id") as string;
+    const todoId = req.params.id;
+
+    const updatedTodo = await updateTodoById(todoId, req.params);
+
+    if (!updatedTodo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    return res.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(500);
+  }
+}
---
+++ server/src/controller/users.ts
@@ -0,0 +1,56 @@
+import express from "express";
+
+import { deleteUserById, getUsers, getUserById } from "../service/user";
+import { get } from "lodash";
+
+export async function getUser(req: express.Request, res: express.Response) {
+  console.log("getUser");
+  try {
+    const id = get(req, "identity._id");
+
+    const user = await getUserById(id);
+
+    if (id) return res.status(200).json(user);
+
+    return res.status(404).json({ message: "User not found" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function deleteUser(req: express.Request, res: express.Response) {
+  try {
+    const id = get(req, "identity._id");
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
+
+export async function updateUser(req: express.Request, res: express.Response) {
+  try {
+    const id = get(req, "identity._id");
+    const { firstname, lastname } = req.body;
+
+    if (!firstname || !lastname) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    user.lastname = lastname;
+    user.firstname = firstname;
+
+    await user.save();
+
+    return res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+    return res.sendStatus(400);
+  }
+}
